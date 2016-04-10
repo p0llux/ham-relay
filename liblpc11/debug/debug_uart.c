69d5ece37e0f50ba6872f698ff2eebeabf1978bb
Jean-François Wauthy <jfw@spancircle.be>
2016-04-10 13:11:09 +0200

    We need to reduce the debug baudrate now that we are using the IRC

--- liblpc11/debug/debug_uart.c
+++ liblpc11/debug/debug_uart.c
@@ -7,7 +7,7 @@ debug_init (void)
   Chip_IOCON_PinMuxSet (LPC_IOCON, IOCON_PIO1_7, IOCON_MODE_INACT | IOCON_FUNC1);
 
   Chip_UART_Init (LPC_USART);
-  Chip_UART_SetBaud (LPC_USART, 115200);
+  Chip_UART_SetBaud (LPC_USART, 57600);
   Chip_UART_ConfigData (LPC_USART, (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT));
   Chip_UART_TXEnable (LPC_USART);
 }
